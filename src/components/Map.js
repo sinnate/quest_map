03037f06ee0f8810bb074e168f1e378fa5644028
Sinnate Mohammed Amine <contact@sinnate.dev>
2023-11-07 20:49:01 +0100

    yes

--- src/components/Map.js
+++ src/components/Map.js
@@ -40,7 +40,8 @@ function Map() {
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        // url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
 
